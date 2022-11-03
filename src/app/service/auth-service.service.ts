2c1be7609117a4f27f3a99cd2073d8191f58901c
edgarbm16 <2c.edgar.barron@gmail.com>
2022-11-03 00:01:56 -0600

    Registro manda a login y diseño de login

--- src/app/service/auth-service.service.ts
+++ src/app/service/auth-service.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
+import { environment as ENV } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+
+export interface AuthLogin {
+  email?: string;
+  password?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthServiceService {
+
+  private url = ENV.API + 'users';
+  private header = {
+    "X-Parse-Application-Id": ENV.APP_ID,
+    "X-parse-REST-API-Key": ENV.API_SECRET,
+    "X-Parse-Revocable-Session": "1",
+    "Content-Type": "application/json"
+  }
 
   constructor(
     private http: HttpClient
   ) { }
 
 
-  authService(){
-    return 
+  authService(authlogin: AuthLogin){
+    return this.http.get();
   }
 }
 
